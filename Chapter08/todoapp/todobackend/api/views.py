db94b5dd5b00e3b7e5af3ee2d2c1a013d9f84ea1
Daniel Correa <yo@danielgara.com>
2022-01-29 12:37:56 -0500

    imp: Chapter08

--- Chapter08/todoapp/todobackend/api/views.py
+++ Chapter08/todoapp/todobackend/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .serializers import TodoSerializer
 from todo.models import Todo
 
@@ -6,6 +6,7 @@ class TodoListCreate(generics.ListCreateAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and # queryset.
     # We specify TodoSerializer which we have earlier implemented
     serializer_class = TodoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user        
