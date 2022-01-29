d597445fd2a09c042ee00ffcce783cdd98c8d9d0
Daniel Correa <yo@danielgara.com>
2022-01-29 12:30:13 -0500

    imp: pre-chapter08

---
+++ Chapter08/todoapp/todobackend/api/views.py
@@ -0,0 +1,16 @@
+from rest_framework import generics
+from .serializers import TodoSerializer
+from todo.models import Todo
+
+class TodoListCreate(generics.ListCreateAPIView):
+    # ListAPIView requires two mandatory attributes, serializer_class and # queryset.
+    # We specify TodoSerializer which we have earlier implemented
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user        
+        return Todo.objects.filter(user=user).order_by('-created')
+    
+    def perform_create(self, serializer):
+        #serializer holds a django model
+        serializer.save(user=self.request.user) 
