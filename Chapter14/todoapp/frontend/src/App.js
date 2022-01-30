3246568e612dabae8d848ee281a16329cefc485e
Daniel Correa <yo@danielgara.com>
2022-01-30 11:17:23 -0500

    imp: pre-chapter14

---
+++ Chapter14/todoapp/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      Hello World
+    </div>
+  );
+}
+
+export default App;
---
+++ Chapter14/todoapp/todobackend/api/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('todos/', views.TodoListCreate.as_view()),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view()),
+    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view()),
+    path('signup/', views.signup),
+    path('login/', views.login),
+]
