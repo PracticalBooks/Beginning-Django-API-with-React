49e30488f85c0fa895db1cb4f05a8cea2a24fa0e
Daniel Correa <yo@danielgara.com>
2022-02-04 10:08:03 -0500

    imp: Chapter18

--- Chapter18/todoapp/frontend/src/components/todos-list.js
+++ Chapter18/todoapp/frontend/src/components/todos-list.js
@@ -1,10 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import TodoDataService from '../services/todos';
+import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Alert from 'react-bootstrap/Alert';
+import moment from 'moment';
 
-function TodosList() {
+const TodosList = props => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    retrieveTodos();
+  }, [props.token]);
+
+  const retrieveTodos = () => {
+    TodoDataService.getAll(props.token)
+      .then(response => {
+        setTodos(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
   return (
-    <div className="App">
-      Todos List
-    </div>
+    <Container>
+      {props.token == null || props.token === "" ? (
+        <Alert variant='warning'>
+          You are not logged in. Please <Link to={"/login"}>login</Link> to see your todos.
+        </Alert>
+      ) : (
+        <div>
+          {todos.map((todo) => {
+            return (
+              <Card key={todo.id} className="mb-3">
+                <Card.Body>
+                  <div>
+                    <Card.Title>{todo.title}</Card.Title>
+                    <Card.Text><b>Memo:</b> {todo.memo}</Card.Text>
+                    <Card.Text>
+                      Date created: {moment(todo.created).format("Do MMMM YYYY")}
+                    </Card.Text>
+                  </div>
+                  <Link to={{
+                    pathname: "/todos/" + todo.id,
+                    state: {
+                      currentTodo: todo
+                    }
+                  }}>
+                    <Button variant="outline-info" className="me-2">
+                      Edit
+                    </Button>
+                  </Link>
+                  <Button variant="outline-danger">
+                    Delete
+                  </Button>
+                </Card.Body>
+              </Card>
+            )
+          })}
+        </div>
+      )}
+    </Container>
   );
 }
 
